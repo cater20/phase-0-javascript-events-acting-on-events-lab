797afb3a68ca6f89d3369d848f00f38a9365cf5e
ketah <onyangoketta@gmail.com>
2022-06-14 17:49:07 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+//dodger.style.left="180px"
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+   const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+     dodger.style.left = `${left - 1}px`;
+   }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  false
+
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+   const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+     dodger.style.left = `${left + 1}px`;
+   }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Arrowup") {
+      moveDodgerRight();
+    }
+  });
+  false
